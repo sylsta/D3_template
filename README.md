048525c9262ed7ef44fdc9e46a6bf4d3701e7570
Sylvain <sylvain@thery.org>
2021-01-17 17:28:31 +0100

    starting...

---
+++ README.md
@@ -0,0 +1,2 @@
+## D3 Template files for starting D3 projects
+Taken from https://www.datavis.fr/index.php?page=firststep
---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ${USER} on ${DATE}.
+ */
+var svg = d3.select("#svg1").append("svg");
+svg.append("rect")
+    .attr("x",20)
+    .attr("y",20)
+    .attr("width",200)
+    .attr("height",50);
